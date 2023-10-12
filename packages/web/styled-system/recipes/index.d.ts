047afcec1030b817204fdd5436bee3da4f9b9ac2
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-10-12 23:14:09 +0700

    Remove styled-system

--- packages/web/styled-system/recipes/index.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-export * from './button';
-export * from './input';
-export * from './label';
-export * from './avatar';
-export * from './pagination';
-export * from './switches';
-export * from './slider';
--- packages/web/styled-system/recipes/index.mjs
+++
@@ -1,7 +0,0 @@
-export * from './button.mjs';
-export * from './input.mjs';
-export * from './label.mjs';
-export * from './avatar.mjs';
-export * from './pagination.mjs';
-export * from './switches.mjs';
-export * from './slider.mjs';
