83ec0c8d9469b273e9163d7dfe138632df7bc659
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-09-28 21:43:20 +0700

    deps: upgrade pandacss dependencies

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,20 +2,18 @@ import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import solidJs from '@astrojs/solid-js'
 import vue from '@astrojs/vue'
-import pandacss from '@pandacss/astro'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react({
-      include: ['**/react/*'],
-    }),
-    vue(),
-    solidJs({
-      include: ['**/solid/*'],
-    }),
-    mdx(),
-    pandacss(),
-  ],
+	integrations: [
+		react({
+			include: ['**/react/*'],
+		}),
+		vue(),
+		solidJs({
+			include: ['**/solid/*'],
+		}),
+		mdx(),
+	],
 })
--- apps/web/panda.config.ts
+++ apps/web/panda.config.ts
@@ -4,10 +4,7 @@ import pagaPreset from '../../packages/preset/src/index'
 export default defineConfig({
 	preflight: true,
 	presets: ['@pandacss/dev/presets', pagaPreset],
-	include: [
-		'./src/**/*.{js,jsx,ts,tsx,vue,astro}',
-		'./pages/**/*.{js,jsx,ts,tsx,vue,astro}',
-	],
+	include: ['./src/**/*.{js,jsx,ts,tsx,vue,astro}'],
 	exclude: [],
 	theme: {
 		extend: {},
