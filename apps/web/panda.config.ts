4efeb9cc4bb7460566c517ac7ca420f4eec99c34
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-10-19 16:14:00 +0700

    chore: update paga preset

--- apps/web/panda.config.ts
+++ apps/web/panda.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@pandacss/dev'
 export default defineConfig({
 	preflight: true,
 	lightningcss: true,
-	presets: ['@pandacss/dev/presets', '@paga/preset'],
+	presets: ['@pandacss/preset-panda', '@paga/preset'],
 	include: ['./src/**/*.{js,jsx,ts,tsx,vue,astro}'],
 	exclude: [],
 	theme: {
